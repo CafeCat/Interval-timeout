7c2603cb783aa444efbfef0acfafebd3ef8f6858
Leonore Yardimli <leonore1982@gmail.com>
2016-05-11 16:48:36 +0800

    mac: test post-merge

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 //test comment 2
 // test comment 3
 //test comment 4
+//test post-merge
 $(document).ready(function() {  
   var boxTemplate =  $('.box-container').find('.box').prop('outerHTML');
   function setupView(num){
