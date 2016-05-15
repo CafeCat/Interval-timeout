ce39a8651b362f131ed6f329a4e6621d8554cef1
Leonore Yardimli <leonore1982@gmail.com>
2016-05-16 01:50:39 +0800

    test auto pull again

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
 //test webhook6
 //test webhook7
 //test webhook8
+//test webhook9
 $(document).ready(function() {  
   var boxTemplate =  $('.box-container').find('.box').prop('outerHTML');
   function setupView(num){
